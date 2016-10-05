30f345503770f2ff9d81b29f0225f70ebd120416
pancw <549191530@qq.com>
2016-10-05 13:32:18 +0000

    client tick

--- server/src/client.lua
+++ server/src/client.lua
@@ -16,13 +16,18 @@ function client:_init(vfd)
 	--logic
 	self.x = 0
 	self.y = 0
-	self.v = 3
+	self.v = 0
 	self.r = 'right'
 	self.press = false
+	self.angle = 90
+	self.d = 1
 end
 
 function client:cmd(cmd)
 	for k, v in pairs(cmd) do
 		self[k] = v
+	end
+	if cmd.press then
+		self.d = self.d * -1
 	end
 end
--- server/src/module/battle_room.lua
+++ server/src/module/battle_room.lua
@@ -1,21 +1,18 @@
+local ANGLE = 3
 function tick()
 	local data = {}
 	for vfd, client in pairs(clients) do
 		local v = client.v
-		if client.r == "right" then
-			client.x = client.x + v
-		elseif client.r == "left" then
-			client.x = client.x - v
-		elseif client.r == "up" then
-			client.y = client.y + v
-		elseif client.r == "down" then
-			client.y = client.y - v
-		end
 
 		if client.press then
+			local a = client.angle/180*math.pi
+			client.x = client.x + v*math.cos(a)
+			client.y = client.y + v*math.sin(a)
+
 			client.v = client.v + 1	
 		else
 			client.v = client.v - 1
+			client.angle = (client.angle + ANGLE*client.d)%360
 		end
 		if client.v < 0 then
 			client.v = 0
@@ -26,6 +23,7 @@ function tick()
 			y = client.y,
 			r = client.r,
 			v = client.v,
+			angle = client.angle,
 		}
 	end
 
